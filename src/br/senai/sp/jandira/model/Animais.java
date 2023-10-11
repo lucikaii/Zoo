593071710ba5fb9585d3d76eeeb00889a9029b7d
Bragakaii <bragaluciano.nan@gmail.com>
2023-10-11 10:54:49 -0300

    cadastro em andamento...

---
+++ src/br/senai/sp/jandira/model/Animais.java
@@ -0,0 +1,10 @@
+package br.senai.sp.jandira.model;
+
+public class Animais {
+
+    String especie;
+    String nome;
+    int idade;
+    String sexo;
+    int id_animal;
+}
---
+++ src/br/senai/sp/jandira/model/Mamiferos.java
@@ -0,0 +1,35 @@
+package br.senai.sp.jandira.model;
+
+import java.util.Scanner;
+
+public class Mamiferos extends Animais{
+
+    Scanner keyboard = new Scanner(System.in);
+
+    public void cadastrarFelinos(){
+
+        System.out.println("---------- CADASTRAR FELINO ----------");
+        System.out.println("");
+        System.out.println("Informe o nome: ");
+        super.nome = keyboard.nextLine();
+        System.out.println("Informe a especie: ");
+        super.especie = keyboard.nextLine();
+        System.out.println("Informe o sexo [Masc/Fem]: ");
+        super.sexo = keyboard.nextLine();
+        System.out.println("Informe a idade: ");
+        super.idade = keyboard.nextInt();
+        System.out.println("Informe o ID do felino:");
+    }
+
+    public void cadastrarCetaceos(){
+
+        System.out.println("---------- CADASTRAR CETÁCEO ----------");
+        System.out.println("");
+        System.out.println("Informe o nome: ");
+        super.nome = keyboard.nextLine();
+        System.out.println("Informe a espécie: ");
+        super.especie = keyboard.nextLine();
+        System.out.println("Informe o sexo [Masc/Fem]: ");
+        super.sexo =
+    }
+}
